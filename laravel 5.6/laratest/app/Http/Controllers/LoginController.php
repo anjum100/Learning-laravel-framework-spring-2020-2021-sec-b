42c407ee4412d72f39a050f75f5420561f6bb6f6
unknown <anjummishu8@gmail.com>
2021-02-19 01:48:04 +0600

    update

--- laravel 5.6/laratest/app/Http/Controllers/LoginController.php
+++ laravel 5.6/laratest/app/Http/Controllers/LoginController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
+use App\User;
+use Illuminate\Support\Facades\DB;
 
 class LoginController extends Controller
 {
@@ -12,22 +14,33 @@ class LoginController extends Controller
 
     public function verify(Request $req){
 
+        /* $user = User::where('password', $req->password)
+                    ->where('username', $req->username)
+                    ->get(); */
+        //echo count($user);
+
+        $user = DB::table('user_table')
+                    ->where('password', $req->password)
+                    ->where('username', $req->username)
+                    ->get();
+
+
         if($req->username == "" || $req->password == ""){
            $req->session()->flash('msg', 'null username or password...');
            return redirect('/login');
 
-        }elseif($req->username == $req->password){
+        }elseif(count($user) > 0 ){
             //$req->session()->put('password', $req->password);
             //$req->session()->get('name');
             //$req->session()->forget('name');
             //$req->session()->flush();
             //$req->session()->has('name');
 
-            /* $req->session()->flash('msg', 'Invalid user info...');
-            $req->session()->flash('error', 'Bad request error...');
-            $req->session()->get('msg');
-            $req->session()->keep('msg');
-            $req->session()->get('msg'); */
+            //$req->session()->flash('msg', 'Invalid user info...');
+            //$req->session()->flash('error', 'Bad request error...');
+            //$req->session()->get('msg');
+            //$req->session()->keep('msg');
+            //$req->session()->get('msg');
             //$req->session()->reflash();
             //$req->session()->pull('name');
 
